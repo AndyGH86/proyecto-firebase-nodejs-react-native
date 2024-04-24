e678ff14533e1ddd46977a5e95a53619ac28dde6
Anderson Gonzalez <141663667+AndyGH86@users.noreply.github.com>
2024-04-24 08:56:34 +0000

    creando vistas

--- proyecto-app/App.js
+++ proyecto-app/App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+
+function myStack() {
+  return(
+  <Stack.Navigator>
+    <Stack.Screen />
+  </Stack.Navigator>
+
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+
+    </NavigationContainer>
   );
 }
 
---
+++ proyecto-app/vista/vistalibors.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const BooksList = () => {
+    return(
+        <View>
+            <Text>Books List</Text>
+        </View>
+
+    )
+
+
+}
