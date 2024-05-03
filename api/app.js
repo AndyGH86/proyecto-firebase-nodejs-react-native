c02a242fdd290e5e65f179a863cadf7f3008a76a
AndyGH86 <141663667+AndyGH86@users.noreply.github.com>
2024-05-03 08:26:13 +0200

    delete

--- api/app.js
+++ api/app.js
@@ -8,14 +8,11 @@ const cors = require('cors')
 
 const app = express();
 const port = 3000
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 
 
